845fc8e087ae74cd568f7be65580037351fb1489
Gábor Csárdi <csardi.gabor@gmail.com>
2022-04-29 10:22:08 +0200

    Avoid R CMD check warning for library()

--- R/roxy_utils.R
+++ R/roxy_utils.R
@@ -21,7 +21,7 @@ format.rd_section_tip <- function(x, ...) {
 
 .onLoad <- function(libname, pkgname) {
   if (isNamespaceLoaded("roxygen2")) {
-    library(roxygen2)
+    attachNamespace("roxygen2")
     registerS3method("roxy_tag_parse", "roxy_tag_tip", roxy_tag_parse.roxy_tag_tip)
     registerS3method("roxy_tag_rd", "roxy_tag_tip", roxy_tag_rd.roxy_tag_tip)
   }
